bd5cc376fffff92d418f448e21074b7089a8505b
golle404 <golle@inbox.com>
2016-01-02 15:29:30 +0100

    favicon

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 var express = require("express");
+var favicon = require('serve-favicon');
+
 var path = require("path");
 
 var routes = require("./app/routes/routes")
@@ -10,6 +12,8 @@ var app = express();
 // using jade as template engine
 app.set('views', path.join(__dirname, '/app/views'));
 app.set("view engine", "jade");
+// favicon
+app.use(favicon(__dirname + '/public/favicon.png'));
 
 // router
 app.use('/', routes);
